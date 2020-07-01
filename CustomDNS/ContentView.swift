527978fd49a9ab3ebb1129d7ea122f77f1ccb8ba
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2020-07-01 21:46:36 +0900

    Initial Commit

---
+++ CustomDNS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CustomDNS
+//
+//  Created by Kenta Kubo on 7/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomDNS/CustomDNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDNSApp.swift
+//  CustomDNS
+//
+//  Created by Kenta Kubo on 7/1/20.
+//
+
+import SwiftUI
+
+@main
+struct CustomDNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

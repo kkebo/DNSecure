7579780886cc0f2569237999f12f43de9c15e954
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2020-07-23 23:21:26 +0900

    Add the implementation that configures 1.1.1.1 to NEDNSSettingsManager

--- CustomDNS/ContentView.swift
+++ CustomDNS/ContentView.swift
@@ -5,11 +5,39 @@
 //  Created by Kenta Kubo on 7/1/20.
 //
 
+import Combine
+import NetworkExtension
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!").padding()
+struct ContentView {
+    @State var isEnabled = false
+    var cancellable: Cancellable?
+
+    init() {
+        self.cancellable = Future<Bool, Error> { resolve in
+            let manager = NEDNSSettingsManager.shared()
+            manager.loadFromPreferences {
+                if let err = $0 {
+                    resolve(.failure(err))
+                } else {
+                    resolve(.success(manager.isEnabled))
+                }
+            }
+        }
+        .mapError { fatalError("\($0.localizedDescription)") }
+        .assign(to: \.isEnabled, on: self)
+    }
+}
+
+extension ContentView: View {
+    @ViewBuilder var body: some View {
+        if self.isEnabled {
+            Text("Enabled")
+                .foregroundColor(.green)
+        } else {
+            Text("Disabled")
+                .foregroundColor(.secondary)
+        }
     }
 }
 
--- CustomDNS/CustomDNSApp.swift
+++ CustomDNS/CustomDNSApp.swift
@@ -5,10 +5,39 @@
 //  Created by Kenta Kubo on 7/1/20.
 //
 
+import NetworkExtension
 import SwiftUI
 
 @main
-struct CustomDNSApp: App {
+struct CustomDNSApp {
+    init() {
+        // Create a DNS configuration
+        let manager = NEDNSSettingsManager.shared()
+        manager.loadFromPreferences { loadError in
+            if let loadError = loadError {
+                print(loadError)
+                return
+            }
+            let dotSettings = NEDNSOverTLSSettings(servers: [
+                "1.1.1.1",
+                "1.0.0.1",
+                "2606:4700:4700::64",
+                "2606:4700:4700::6400",
+            ])
+            dotSettings.serverName = "cloudflare-dns.com"
+            manager.dnsSettings = dotSettings
+            manager.saveToPreferences { saveError in
+                if let saveError = saveError {
+                    print(saveError)
+                    return
+                }
+                print("saved")
+            }
+        }
+    }
+}
+
+extension CustomDNSApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()

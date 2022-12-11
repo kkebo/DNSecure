bc678c3fa833279ecc0d0f290df385799a792fb2
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2022-12-12 01:14:58 +0900

    Fix navigation issues on iOS 16, iPadOS 16, and Mac Catalyst

--- DNSecure/Views/DetailView.swift
+++ DNSecure/Views/DetailView.swift
@@ -21,6 +21,57 @@ struct DetailView {
 
 extension DetailView: View {
     var body: some View {
+        if #available(iOS 16, *) {
+            self.modernBody
+        } else {
+            self.legacyBody
+        }
+    }
+
+    @available(iOS 16, *)
+    private var modernBody: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    Toggle("Use This Server", isOn: self.$isOn)
+                }
+                Section {
+                    HStack {
+                        Text("Name")
+                        TextField("Name", text: self.$server.name)
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+                self.serverConfigurationSections
+                Section {
+                    ForEach(self.server.onDemandRules) { rule in
+                        NavigationLink(rule.name, value: rule)
+                    }
+                    .onDelete { self.server.onDemandRules.remove(atOffsets: $0) }
+                    .onMove { self.server.onDemandRules.move(fromOffsets: $0, toOffset: $1) }
+                    Button("Add New Rule") {
+                        self.server.onDemandRules
+                            .append(OnDemandRule(name: "New Rule"))
+                    }
+                } header: {
+                    EditButton()
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .overlay(alignment: .leading) {
+                            Text("On Demand Rules")
+                        }
+                }
+            }
+            .navigationDestination(for: OnDemandRule.self) { rule in
+                // When RuleView is opened and tap another server on the sidebar, the previous server's rule comes here.
+                if self.server.onDemandRules.contains(rule) {
+                    RuleView(rule: self.binding(for: rule))
+                }
+            }
+        }
+        .navigationTitle(self.server.name)
+    }
+
+    private var legacyBody: some View {
         Form {
             Section {
                 Toggle("Use This Server", isOn: self.$isOn)

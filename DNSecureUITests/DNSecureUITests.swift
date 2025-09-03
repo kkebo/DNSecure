8dd0f2d50ed5d80f36f5402ed746889d98183174
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2025-09-03 22:46:05 +0900

    chore: update Xcode project to the latest settings

--- DNSecureUITests/DNSecureUITests.swift
+++ DNSecureUITests/DNSecureUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class DNSecureUITests: XCTestCase {
+final class DNSecureUITests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -21,21 +21,20 @@ class DNSecureUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    @MainActor
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
-        // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    @MainActor
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+        // This measures how long it takes to launch your application.
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            XCUIApplication().launch()
         }
     }
 }
---
+++ DNSecureUITests/DNSecureUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DNSecureUITestsLaunchTests.swift
+//  DNSecureUITests
+//
+//  Created by Kenta Kubo on 9/3/25.
+//
+
+import XCTest
+
+final class DNSecureUITestsLaunchTests: XCTestCase {
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
